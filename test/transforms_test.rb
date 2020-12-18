ae5fc21a9005e550d26e5d5449df51e5e62daba7
Иван Разуваев <i.razuvaev@zonatelecom.ru>
2020-12-18 12:01:03 +0000

    fixed melspec test

--- test/transforms_test.rb
+++ test/transforms_test.rb
@@ -11,9 +11,9 @@ class TransformsTest < Minitest::Test
 
   def test_melspectrogram
     waveform, sample_rate = TorchAudio.load(audio_path)
-    transformed = TorchAudio::Transforms::MelSpectrogram.new.call(waveform)
+    transformed = TorchAudio::Transforms::MelSpectrogram.new(sample_rate: sample_rate).call(waveform)
     assert_equal [1, 128, 255], transformed.size
-    expected = [0, 0, 0, 0, 0]
+    expected = [4.320904736232478e-06, 0.00026097119553014636, 0.00010256850509904325, 0.0009344223653897643, 0.00013253440556582063]
     assert_elements_in_delta expected, transformed[0][0][0..4].to_a
   end
   
