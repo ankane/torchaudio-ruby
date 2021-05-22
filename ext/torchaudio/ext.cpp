aadb09e24166d655823a254f06439c61076380d0
Andrew Kane <andrew@ankane.org>
2021-05-21 23:59:02 -0700

    Simplified code [skip ci]

--- ext/torchaudio/ext.cpp
+++ ext/torchaudio/ext.cpp
@@ -3,79 +3,25 @@
 #include <rice/rice.hpp>
 #include <rice/stl.hpp>
 
-using namespace Rice;
-
-class SignalInfo {
-  sox_signalinfo_t* value = nullptr;
-  public:
-    SignalInfo(VALUE v) {
-      if (v != Qnil) {
-        value = Rice::detail::From_Ruby<sox_signalinfo_t*>().convert(v);
-      }
-    }
-    operator sox_signalinfo_t*() {
-      return value;
-    }
-};
-
-namespace Rice::detail
-{
-  template<>
-  class From_Ruby<SignalInfo>
-  {
-  public:
-    SignalInfo convert(VALUE x)
-    {
-      return SignalInfo(x);
-    }
-  };
-}
-
-class EncodingInfo {
-  sox_encodinginfo_t* value = nullptr;
-  public:
-    EncodingInfo(VALUE v) {
-      if (v != Qnil) {
-        value = Rice::detail::From_Ruby<sox_encodinginfo_t*>().convert(v);
-      }
-    }
-    operator sox_encodinginfo_t*() {
-      return value;
-    }
-};
-
-namespace Rice::detail
-{
-  template<>
-  class From_Ruby<EncodingInfo>
-  {
-  public:
-    EncodingInfo convert(VALUE x)
-    {
-      return EncodingInfo(x);
-    }
-  };
-}
-
 extern "C"
 void Init_ext()
 {
-  Module rb_mTorchAudio = define_module("TorchAudio");
+  auto rb_mTorchAudio = Rice::define_module("TorchAudio");
 
-  Module rb_mExt = define_module_under(rb_mTorchAudio, "Ext")
+  auto rb_mExt = Rice::define_module_under(rb_mTorchAudio, "Ext")
     .define_singleton_function(
       "read_audio_file",
-      [](const std::string& file_name, at::Tensor output, bool ch_first, int64_t nframes, int64_t offset, SignalInfo si, EncodingInfo ei, const char* ft) {
+      [](const std::string& file_name, at::Tensor output, bool ch_first, int64_t nframes, int64_t offset, sox_signalinfo_t* si, sox_encodinginfo_t* ei, const char* ft) {
         return torch::audio::read_audio_file(file_name, output, ch_first, nframes, offset, si, ei, ft);
       })
     .define_singleton_function(
       "write_audio_file",
-      [](const std::string& file_name, const at::Tensor& tensor, SignalInfo si, EncodingInfo ei, const char* file_type) {
+      [](const std::string& file_name, const at::Tensor& tensor, sox_signalinfo_t* si, sox_encodinginfo_t* ei, const char* file_type) {
         return torch::audio::write_audio_file(file_name, tensor, si, ei, file_type);
       });
 
-  Class rb_cSignalInfo = define_class_under<sox_signalinfo_t>(rb_mExt, "SignalInfo")
-    .define_constructor(Constructor<sox_signalinfo_t>())
+  auto rb_cSignalInfo = Rice::define_class_under<sox_signalinfo_t>(rb_mExt, "SignalInfo")
+    .define_constructor(Rice::Constructor<sox_signalinfo_t>())
     .define_method("rate", [](sox_signalinfo_t& self) { return self.rate; })
     .define_method("channels", [](sox_signalinfo_t& self) { return self.channels; })
     .define_method("precision", [](sox_signalinfo_t& self) { return self.precision; })
